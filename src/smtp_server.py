1b7d73b458a059fe481d0d0a08073407654e2915
Gustavo Miranda <65552500+gustav0mirand4@users.noreply.github.com>
2026-06-23 08:47:27 -0300

    Update smtp_server.py

--- src/smtp_server.py
+++ src/smtp_server.py
@@ -8,7 +8,6 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 
-# Carregando as variáveis de ambiente
 load_dotenv()
 
 class SmtpServer:
@@ -18,14 +17,12 @@ class SmtpServer:
         self.email_smtp = os.getenv("SMTPEMAIL")
         self.password_smpt = os.getenv("SMTPPASS")
 
-    # Gerador de código de 4 dígitos 
     def code_generator(self):
         char_list = "ABCDEFGHIJKLMNOPQRSUVXWYZ0123456789"
         chars = random.choices(char_list, k=4)
         news_chars = "".join(chars)
         return news_chars
         
-    # Função para enviar email para o usuário
     def send_email(self, to_stmp_email):
         self.body = f"{self.code_generator()}"
 
@@ -36,16 +33,12 @@ class SmtpServer:
         self.email_message.attach(MIMEText(self.body,"plain"))
 
         try:
-            # Startando servidor STMP
             self.server.starttls()
             self.server.login(self.email_smtp, self.password_smpt)
-
-            # Enviando o email tipo MIME no servidro SMTP
             self.server.sendmail(self.email_message["From"], self.email_message["To"], self.email_message.as_string())
 
         except Exception as error:
             print(f"Erro {error}")
 
-        else:
-            # Fechando o servidor       
+        else:    
             self.server.quit()
